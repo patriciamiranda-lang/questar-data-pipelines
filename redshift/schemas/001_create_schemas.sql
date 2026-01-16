470c5f4d211a9beda94010830c33af092ea8da51
Patricia Miranda <patricia.miranda@distrito.me>
2026-01-16 09:40:26 -0300

    Update 001_create_schemas.sql

--- redshift/schemas/001_create_schemas.sql
+++ redshift/schemas/001_create_schemas.sql
@@ -1,2 +1,9 @@
+-- 001_create_schemas.sql
+-- Cria os schemas base do DW e de administração
+
 CREATE SCHEMA IF NOT EXISTS admin;
 CREATE SCHEMA IF NOT EXISTS dw_redshift;
+
+-- Comentários ajudam muito no Query Editor / governança
+COMMENT ON SCHEMA admin IS 'Objetos administrativos: procedures, lock tables, metadados de PK/FK.';
+COMMENT ON SCHEMA dw_redshift IS 'Data Warehouse (dimensoes, fatos, views e materialized views).';
